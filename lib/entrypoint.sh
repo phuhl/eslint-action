aaa610c8ef11e4075c66c12f4f64bf12f6501ac0
Philipp Uhl <git@ph-uhl.com>
2019-11-19 13:42:11 +0100

    Update entrypoint.sh

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -2,6 +2,6 @@
 
 set -e
 rm package.json
-npm install  eslint eslint-plugin-jsx-a11y eslint-plugin-react
+npm install  eslint eslint-plugin-jsx-a11y eslint-plugin-react @actions/github
 
 NODE_PATH=node_modules node /action/lib/run.js
